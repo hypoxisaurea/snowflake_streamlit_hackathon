47a3cdcefa909a71f4c4d246d0525c414e786849
hypoxisaurea <flickerlight14@naver.com>
2025-04-18 17:56:20 +0900

    Updates UI text formatting in main app

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title("TargetIQ")
-st.subheader("🎯 백화점 방문 예측 및 마케팅 전략 추천 서비스")
+st.title("🎯 TargetIQ")
+st.subheader("백화점 방문 예측 및 마케팅 전략 추천 서비스")
 st.markdown("""
     이 서비스는 고객의 방문 예측과 맞춤형 마케팅 전략을 제시합니다.  
     페르소나를 확인하고, 분석을 시작해보세요!
